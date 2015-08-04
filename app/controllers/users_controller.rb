673317c474e3b1e543a171a5d531ec1173e92319
Rey Christopher <rey.lee@astridtechnologies.com>
2015-08-04 21:30:24 +0800

    Delete Micropost

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -62,6 +62,14 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @micropost.destroy
+    respond_to do |format|
+      format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
